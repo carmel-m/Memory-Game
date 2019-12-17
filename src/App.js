5bd464f4414cd1ed9b52bf6bd25e9492571cb21e
Carmel Meyer <cemeyer@elkera.com>
2019-12-17 10:12:17 -0500

    Add some components

--- src/App.js
+++ src/App.js
@@ -24,3 +24,46 @@ function App() {
 }
 
 export default App;
+
+
+// import React from "react";
+// import FriendCard from "./components/FriendCard";
+// import Wrapper from "./components/Wrapper";
+// import friends from "./friends.json";
+// import "./App.css";
+
+// class App extends React.Component {
+//   // set state to friends.json
+//   state = {
+//     friends
+//   };
+
+//   removeFriend = id => {
+//     const newFriends = this.state.friends.filter(
+//       isClicked => isClicked.id !== id
+//     );
+
+//     this.setState({ friends: newFriends });
+//   }
+
+//   render() {
+//     return (
+//       <Wrapper>
+//         <h1 className="title">Friends List</h1>
+//         {this.state.friends.map(friendInfo => (
+//           <FriendCard
+//             name={friendInfo.name}
+//             image={friendInfo.image}
+//             occupation={friendInfo.occupation}
+//             location={friendInfo.location}
+//             key={friendInfo.id}
+//             id={friendInfo.id}
+//             removeFriend={this.removeFriend}
+//           />
+//         ))}
+//       </Wrapper>
+//     );
+//   }
+// }
+
+// export default App;
