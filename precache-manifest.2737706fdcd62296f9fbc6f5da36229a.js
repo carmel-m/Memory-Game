d649aba7cce54d39639f16f115e866863906dfd5
Carmel Meyer <cemeyer@elkera.com>
2019-12-19 17:43:21 -0500

    Updates

---
+++ precache-manifest.2737706fdcd62296f9fbc6f5da36229a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7a88b9e3129420e457d00394a3cd6a",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "a5b837ed648985ef33f6",
+    "url": "/Memory-Game/static/css/main.44ca8a42.chunk.css"
+  },
+  {
+    "revision": "af90697ec048b5d39544",
+    "url": "/Memory-Game/static/js/2.e1bc4d60.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Memory-Game/static/js/2.e1bc4d60.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5b837ed648985ef33f6",
+    "url": "/Memory-Game/static/js/main.b790ecef.chunk.js"
+  },
+  {
+    "revision": "17890d2f4a0f3d49b3e5",
+    "url": "/Memory-Game/static/js/runtime-main.b2b06367.js"
+  }
+]);
